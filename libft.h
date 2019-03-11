433149a4f9f23063b2b2ed64bb1fdbfa63a71abd
Eisuke Esaki <eesaki@e1z2r6p5.42.us.org>
2019-03-11 10:48:16 -0700

    completed: isalnum, refactored: isalpha, fixed:...
    fixed:
    	- libft.h typo
    
    updated:
    	- Makefile

--- libft.h
+++ libft.h
@@ -4,7 +4,8 @@
 
 int		ft_iswhitespace(char c);
 int		ft_isdigit(int c);
-int		isalpha(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_atoi(const char *str);
 char	*ft_strcpy(char *dst, const char *src);
