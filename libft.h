897f1bf7f90cce3cba843ce22c45599582b9029e
Eisuke Esaki <eesaki@e1z2r6p5.42.us.org>
2019-03-11 10:57:52 -0700

    completed: isascii, updated: Makefile, libft.h

--- libft.h
+++ libft.h
@@ -7,6 +7,7 @@ int		ft_isdigit(int c);
 int		ft_isalpha(int c);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
+int		ft_isascii(int c);
 int		ft_atoi(const char *str);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strncpy(char *dst, char *src, size_t n);
