6ce96828ba885cfce99b49861a0b5b15a885d7c8
Eisuke Esaki <eesaki@e1z2r6p5.42.us.org>
2019-03-11 11:30:05 -0700

    completed: toupper, updated: Makefile, libft.h

--- libft.h
+++ libft.h
@@ -9,6 +9,7 @@ int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_toupper(int c);
 int		ft_atoi(const char *str);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strncpy(char *dst, char *src, size_t n);
