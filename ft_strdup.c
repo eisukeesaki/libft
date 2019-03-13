42e56e83391344f32c19aa1c99b204414842441b
Eisuke Esaki <eesaki@e1z2r6p5.42.us.org>
2019-03-12 20:32:19 -0700

    added temporary code to ft_strdup.c
    (to bypass Maintest function dependency issues)

--- ft_strdup.c
+++ ft_strdup.c
@@ -15,6 +15,24 @@
 // #include <string.h>
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>debug purpose
 
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Maintest bypass purpose pbondoer
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	len;
+	char	*s2;
+
+	len = ft_strlen(s1) + 1;
+	s2 = (char *)malloc(sizeof(char) * len);
+	if (s2 == NULL)
+		return (NULL);
+	ft_strcpy(s2, s1);
+	return (s2);
+}
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Maintest bypass purpose pbondoer
+
 // char *ft_strdup(const char *s1)
 // {
 
