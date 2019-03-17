f7b841309757f2144540ccfaa4e236a67ddca2ac
Eisuke Esaki <eesaki@e1z3r8p8.42.us.org>
2019-03-16 18:03:52 -0700

    completed: memchr, in prog: memccpy, updated: ...
    updated:
    	- Makefile
    	- libft.h

---
+++ ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/16 15:44:30 by eesaki            #+#    #+#             */
+/*   Updated: 2019/03/16 18:03:26 by eesaki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char *src;
+
+	src = (unsigned char *)s;
+	while (n--)
+	{
+		if (*src == (unsigned char)c)
+			return (src);
+		src++;
+	}
+	return (NULL);
+}
