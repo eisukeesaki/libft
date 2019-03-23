95decb5c4d4a6465a757d7fcbdca20318a58f995
eisuke <izkyyy@gmail.com>
2019-03-23 11:26:51 -0700

    in progress: strnew

--- libft.h
+++ libft.h
@@ -47,5 +47,6 @@ void	ft_strclr(char *s);
 int		ft_strequ(char const *s1, char const *s2);
 int		ft_strnequ(char const *s1, char const *s2, size_t n);
 void	* ft_memalloc(size_t size);
+char	* ft_strnew(size_t size);
 
 #endif
