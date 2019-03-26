7224e5120009009b2929424c03731916369280ef
Eisuke Esaki <eesaki@e1z2r6p5.42.us.org>
2019-03-25 20:05:42 -0700

    in progress: itoa

--- libft.h
+++ libft.h
@@ -55,5 +55,6 @@ char	*ft_strmap(char const *s, char (*f)(char));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_itoa(int n);
 
 #endif
