a62c6b8e164280b12e8f408c86b503dd76866152
Eisuke Esaki <eesaki@e1z2r6p5.42.us.org>
2019-04-08 17:17:04 -0700

    refactored: strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,18 +6,11 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/11 12:34:24 by eesaki            #+#    #+#             */
-/*   Updated: 2019/03/11 12:34:25 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/04/08 16:19:41 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<debug purpose
-// #include <stdio.h>
-// #include <string.h>
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>debug purpose
-
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Maintest bypass purpose pbondoer
 #include "libft.h"
-#include <stdlib.h>
 
 char	*ft_strdup(const char *s1)
 {
@@ -25,26 +18,9 @@ char	*ft_strdup(const char *s1)
 	char	*s2;
 
 	len = ft_strlen(s1) + 1;
-	s2 = (char *)malloc(sizeof(char) * len);
+	s2 = malloc(sizeof(char) * len);
 	if (s2 == NULL)
 		return (NULL);
 	ft_strcpy(s2, s1);
 	return (s2);
 }
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Maintest bypass purpose pbondoer
-
-// char *ft_strdup(const char *s1)
-// {
-
-// }
-
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<test purpose
-// int		main(void)
-// {
-// 	char str[4] = "abc";
-
-// 	printf("libc:\nstring->%s\n   str->%p\nstrdup->%p\n", strdup(str), str, strdup(str));
-
-// 	return (0);
-// }
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>test purpose
