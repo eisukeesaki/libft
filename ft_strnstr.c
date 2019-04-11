2bde585eccf759bdde9b47ae6dba692027b10e8d
Eisuke Esaki <eesaki@e1z2r6p5.42.us.org>
2019-04-11 16:04:00 -0700

    fixed strnstr bug
    - where not passing unittest4@moulitest

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,76 +6,33 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/11 12:56:19 by eesaki            #+#    #+#             */
-/*   Updated: 2019/04/10 19:57:28 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/04/11 16:03:16 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	h;
 	size_t	n;
+	size_t	nlen;
 
-	if (!*needle)
+	if (*needle == '\0')
 		return ((char *)haystack);
-
+	nlen = ft_strlen(needle);
 	h = 0;
 	n = 0;
-	while (haystack[h] && h < len)
+	while (haystack[h] && len >= nlen)
 	{
-		while (needle[n] && (haystack[h] == needle[n]) && h < len)
-		{
-			h++;
-			n++;
-		}
-		if (h == len) // fix how to check match. use strncmp?
-			return ((char *)haystack);
-		else
-			haystack = &haystack[h];
-		n = 0;
+		if
+		(
+			haystack[h] == needle[n] &&
+			ft_strncmp(&haystack[h], &needle[n], nlen) == 0
+		)
+			return ((char *)&haystack[h]);
+		len--;
 		h++;
 	}
 	return (NULL);
 }
-
-int		main(void)
-{
-	char	buf2[] = "ozarabozaraboze123";
-
-	printf("%s\n", ft_strnstr(buf2, "ozaraboze", 15));
-	// if (strcmp(ft_strnstr(buf2, "ozaraboze", 15), "ozaraboze123") == 0)
-	// 	printf("test success\n");
-	// else
-	// 	printf("test fail\n");
-	return (0);
-}
-
-// char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
-// {
-// 	char		*str;
-// 	char		*sbstr;
-// 	size_t		i;
-
-// 	i = 0;
-// 	if (!*needle)
-// 		return ((char *)haystack);
-	
-// 	while (*haystack && i < len)
-// 	{
-// 		str = (char *)haystack;
-// 		sbstr = (char *)needle;
-// 		while ((*sbstr && *str == *sbstr) && i < len)
-// 		{
-// 			str++;
-// 			sbstr++;
-// 			i++;
-// 		}
-// 		if (*sbstr == '\0')
-// 			return ((char *)haystack);
-// 		haystack++;
-// 		i++;
-// 	}
-// 	return (NULL);
-// }
