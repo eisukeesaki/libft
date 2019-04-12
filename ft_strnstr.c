a78c0b980c69d6fbde4e129b09d7c692c6c2efce
Eisuke Esaki <eesaki@e1z2r5p16.42.us.org>
2019-04-12 16:03:29 -0700

    changed prototype, variable names

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,27 +6,29 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/11 12:56:19 by eesaki            #+#    #+#             */
-/*   Updated: 2019/04/12 13:46:45 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/04/12 15:54:57 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *hs, const char *nd, size_t len)
+char				*ft_strnstr(const char *haystack, const char *needle,
+																size_t len)
 {
 	size_t	h;
 	size_t	n;
 	size_t	nlen;
 
-	if (*nd == '\0')
-		return ((char *)hs);
-	nlen = ft_strlen(nd);
+	if (*needle == '\0')
+		return ((char *)haystack);
+	nlen = ft_strlen(needle);
 	h = 0;
 	n = 0;
-	while (hs[h] && len >= nlen)
+	while (haystack[h] && len >= nlen)
 	{
-		if (hs[h] == nd[n] && ft_strncmp(&hs[h], &nd[n], nlen) == 0)
-			return ((char *)&hs[h]);
+		if (haystack[h] == needle[n] &&
+			ft_strncmp(&haystack[h], &needle[n], nlen) == 0)
+			return ((char *)&haystack[h]);
 		len--;
 		h++;
 	}
