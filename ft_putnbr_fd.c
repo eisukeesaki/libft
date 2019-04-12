f8e308ab3d2c982b29aae2fd8f66903181cf3323
Eisuke Esaki <eesaki@e1z2r5p16.42.us.org>
2019-04-12 13:29:11 -0700

    fixed minor bug

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/21 16:49:41 by eesaki            #+#    #+#             */
-/*   Updated: 2019/03/21 20:00:50 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/04/12 13:26:46 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ inline static void	output(long n, int fd)
 	ft_putchar_fd(n % 10 + '0', fd);
 }
 
-void	ft_putnbr_fd(int n, int fd)
+void				ft_putnbr_fd(int n, int fd)
 {
 	long nb;
 
 	nb = n;
 	if (nb < 0)
 	{
-		ft_putchar('-');
+		ft_putchar_fd('-', fd);
 		nb = -nb;
 	}
 	output(nb, fd);
