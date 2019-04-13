dfed8bebe94966b6af574353d22c2f853837bb08
Eisuke Esaki <eesaki@e1z2r5p16.42.us.org>
2019-04-12 17:01:41 -0700

    added: free list when lstnew fails in loop

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,13 +6,25 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 13:43:09 by eesaki            #+#    #+#             */
-/*   Updated: 2019/04/10 14:55:39 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/04/12 16:59:58 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+inline static void	free_lst(t_list *lst)
+{
+	t_list	*next;
+
+	while (lst != NULL)
+	{
+		next = lst->next;
+		ft_memdel((void **)&lst);
+		lst = next;
+	}
+}
+
+t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list	*new;
 	t_list	*head;
@@ -23,8 +35,13 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	head = new;
 	while (lst->next != NULL)
 	{
+		new->next = ft_lstnew(lst->content, lst->content_size);
+		if (new->next == NULL)
+		{
+			free_lst(head);
+			return (NULL);
+		}
 		lst = lst->next;
-		new->next = f(ft_lstnew(lst->content, lst->content_size));
 		new = new->next;
 	}
 	return (head);
