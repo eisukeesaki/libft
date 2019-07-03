e07c44b4a8fedc832f5235fd767628c1ab681c93
eisuke <izkyyy@gmail.com>
2019-07-03 03:07:29 -0700

    re-implemented ft_atoi from scratch

--- ft_atoi.c
+++ ft_atoi.c
@@ -5,33 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/14 17:24:11 by eesaki            #+#    #+#             */
-/*   Updated: 2019/04/12 13:48:37 by eesaki           ###   ########.fr       */
+/*   Created: 2019/07/03 02:56:20 by eesaki            #+#    #+#             */
+/*   Updated: 2019/07/03 02:57:21 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-	unsigned long long	n;
-	unsigned			neg;
-	unsigned long long	max;
+	int		n;
+	int		i;
+	int		neg;
 
 	n = 0;
-	neg = 0;
-	max = 9223372036854775807;
-	while (ft_iswhitespace(*str))
-		str++;
-	neg = (*str == '-');
-	if (*str == '-' || *str == '+')
-		str++;
-	while (ft_isdigit(*str))
+	i = 0;
+	while (ft_iswhitespace(str[i]))
+		i++;
+	neg = str[i] == '-';
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (ft_isdigit(str[i]))
 	{
-		n = (n * 10) + (*str - '0');
-		str++;
+		n = (n * 10) + (str[i] - '0');
+		i++;
 	}
-	if ((neg == 0 && n > max) || (neg == 1 && n > max + 1))
-		return (neg ? 0 : -1);
 	return (neg ? -n : n);
 }
