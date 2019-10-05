b92ae3cdedce90c712bd3d6d1f0ebee3b76df739
Eisuke Esaki <eesaki@student.42.us.org>
2019-10-04 20:00:16 -0700

    created ft_lstfindlast(), added prototype to header

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/12 13:42:18 by eesaki            #+#    #+#             */
-/*   Updated: 2019/09/16 00:32:34 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/10/04 17:40:00 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,6 +82,8 @@ size_t				ft_words(char const *s, char d);
 void				ft_strdel(char **as);
 t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstadd(t_list **alst, t_list *new);
+void				ft_lstappend(t_list *alst, t_list *new);
+t_list				*ft_lstfindlast(t_list *alst);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
