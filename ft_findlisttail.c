abb01592d813abf4b9be70c239de3d5f9f04a3cc
Eisuke Esaki <eesaki@student.42.us.org>
2019-10-06 20:02:18 -0700

    renamed function name and file, applied necessary changes to related files

--- ft_findlisttail.c
+++ ft_findlisttail.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstfindlast.c                                   :+:      :+:    :+:   */
+/*   ft_findlisttail.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/04 17:35:09 by eesaki            #+#    #+#             */
-/*   Updated: 2019/10/05 15:54:01 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/10/06 17:36:34 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstfindtail(t_list *alst)
+t_list	*ft_findlisttail(t_list *alst)
 {
 	while (alst->next != NULL)
 		alst = alst->next;
